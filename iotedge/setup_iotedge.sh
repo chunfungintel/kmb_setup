e50c7ae0dd47bc5df825ab4ce41de6b5b53e9841
Leow Chun Fung <chun.fung.leow@intel.com>
2020-08-22 03:01:51 +0000

    device connection string autofill

--- iotedge/setup_iotedge.sh
+++ iotedge/setup_iotedge.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "No DEVICE CONNECTION STRING provided, please edit it manually in /etc/iotedge/config.yaml"
+fi
+
+device=$1
+
 SYSTEMD_EDITOR=tee systemctl edit iotedge <<EOF
 [Service]
 Environment="https_proxy=http://proxy-png.intel.com:912"
@@ -21,5 +27,14 @@ chmod +w /etc/iotedge/config.yaml
 HOST=`hostname`
 sed -i "s/<ADD HOSTNAME HERE>/$HOST/" /etc/iotedge/config.yaml
 
+if [ -z "$1" ]; then
+
 echo "Please provide device_connection_string in /etc/iotedge/config.yaml"
 echo "Then run systemctl restart iotedge"
+
+else
+
+sed -i "s/<ADD DEVICE CONNECTION STRING HERE>/$device/" /etc/iotedge/config.yaml
+systemctl restart iotedge
+
+fi
