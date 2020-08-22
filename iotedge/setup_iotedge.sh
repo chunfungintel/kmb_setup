b391ceaeefef2bfc55875cfcda15b13daca47b66
Leow Chun Fung <chun.fung.leow@intel.com>
2020-08-22 03:30:45 +0000

    rearrange

--- iotedge/setup_iotedge.sh
+++ iotedge/setup_iotedge.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+IOTEDGE_CONFIG="/etc/iotedge/config.yaml"
+
 if [ -z "$1" ]; then
-    echo "No DEVICE CONNECTION STRING provided, please edit it manually in /etc/iotedge/config.yaml"
+    echo "No DEVICE CONNECTION STRING provided, please edit it manually in $IOTEDGE_CONFIG"
 fi
 
 device=$1
+
+function SED_FORCE {
+    original=$1
+    replace=$2
+    file=$3
+
+    sed -i "/$original/!{q100} ; s/$original/$replace/" $file
+    if [ $? -eq 100 ]; then
+        echo "Error: $original not found in $file!!!"
+	echo "Error: $replace set failed!!!"
+    fi
+}
 
 SYSTEMD_EDITOR=tee systemctl edit iotedge <<EOF
 [Service]
@@ -25,7 +39,8 @@ systemctl restart docker
 chmod +w /etc/iotedge/config.yaml
 
 HOST=`hostname`
-sed -i "s/<ADD HOSTNAME HERE>/$HOST/" /etc/iotedge/config.yaml
+sed -i "s/<ADD HOSTNAME HERE>/$HOST/" $IOTEDGE_CONFIG
+#SED_FORCE "<ADD HOSTNAME HERE>" $HOST /etc/iotedge/config.yaml
 
 if [ -z "$1" ]; then
 
@@ -34,7 +49,8 @@ echo "Then run systemctl restart iotedge"
 
 else
 
-sed -i "s/<ADD DEVICE CONNECTION STRING HERE>/$device/" /etc/iotedge/config.yaml
+sed -i "s/<ADD DEVICE CONNECTION STRING HERE>/$device/" $IOTEDGE_CONFIG
+#SED_FORCE "<ADD DEVICE CONNECTION STRING HERE>" $device /etc/iotedge/config.yaml
 systemctl restart iotedge
 
 fi
