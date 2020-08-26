b81cc5f89c2c98a14ca1f8e611cdba883a039b7c
Leow Chun Fung <chun.fung.leow@intel.com>
2020-08-26 15:44:34 +0000

    HDDL setup

--- hddl_centos/02_kubernetes.sh
+++ hddl_centos/02_kubernetes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Official guide: https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/"
+
 if [ -z "$1" ]; then
    echo 'Must set hostname!'
    exit
