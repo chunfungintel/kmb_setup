2be1994d9df3c54188b4cf30bae26f10c97bf200
Leow Chun Fung <chun.fung.leow@intel.com>
2020-08-26 15:35:59 +0000

    untainted master

--- k8s/master/create_master_services.sh
+++ k8s/master/create_master_services.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
 kubectl create -f /etc/edge-ai/deploy/0-ros-deploy.yaml; \
 sleep 5; \
 kubectl create -f /etc/edge-ai/deploy/1-device_plugin_daemonset.yaml; \
