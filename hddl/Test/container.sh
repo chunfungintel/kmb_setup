b8082286a5f749f6a3c05aacc68ff3811f06eb8e
swagalex <shubhangix.wagale@intel.com>
2020-09-09 10:26:18 +0800

    kpi test

---
+++ hddl/Test/container.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ -z "$HDDL_IMAGE_NAME" ]; then
+    echo "Need to set HDDL_IMAGE_NAME "
+    exit
+fi
+
+if [ -z "$HDDL_IMAGE_TAG" ]; then
+    echo "Need to set HDDL_IMAGE_TAG "
+    exit
+fi
+
+HDDL_CONTAINER=$HDDL_IMAGE_NAME:$HDDL_IMAGE_TAG
+NAME=TEST
+KPI_START=1
+NUM_CONTAINER=6
+
+sudo chmod 666 /dev/xlnk
+
+for i in $( seq 1 $NUM_CONTAINER )
+do
+    num=$(printf "%02d" $i)
+    CONTAINER_NAME=$NAME-$num
+    echo $CONTAINER_NAME
+    TIME=$((KPI_START*(NUM_CONTAINER -i)))
+    #echo $TIME
+
+    docker run -d \
+        -v /tmp:/tmp \
+        -v /var/tmp:/var/tmp \
+        --device=/dev/xlnk:/dev/xlnk \
+        --env KPI_START=$TIME \
+	$HDDL_CONTAINER /run_kpi.sh
+done
+
+
---
+++ hddl/Test/hddl_service_start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z "$HDDL_IMAGE_NAME" ]; then
+    echo "Need to set HDDL_IMAGE_NAME "
+    exit
+fi
+
+if [ -z "$HDDL_IMAGE_TAG" ]; then
+    echo "Need to set HDDL_IMAGE_TAG "
+    exit
+fi
+
+
+HDDL_CONTAINER=$HDDL_IMAGE_NAME:$HDDL_IMAGE_TAG
+CONTAINER_NAME=TEST01
+KPI_START=5
+
+sudo chmod 666 /dev/xlnk ;\
+docker run -d \
+-v /tmp:/tmp \
+-v /var/tmp:/var/tmp \
+--device=/dev/xlnk:/dev/xlnk \
+--env KPI_START=$KPI_START \
+$HDDL_CONTAINER /run_hddl_service.sh
+
+
