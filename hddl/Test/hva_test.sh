9c66d2b5c72dec6ddfd6317022eedf3338c905eb
Leow, Chun Fung <chun.fung.leow@intel.com>
2020-09-10 05:14:39 -0400

    hva test step

---
+++ hddl/Test/hva_test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$HDDL_IMAGE_NAME" ]; then
+    echo "Need to set HDDL_IMAGE_NAME "
+    exit
+fi
+
+if [ -z "$HDDL_IMAGE_TAG" ]; then
+    echo "Need to set HDDL_IMAGE_TAG "
+    exit
+fi
+
+
+HDDL_CONTAINER=$HDDL_IMAGE_NAME:$HDDL_IMAGE_TAG
+CONTAINER_NAME=TEST01
+KPI_START=5
+
+sudo chmod 666 /dev/xlnk ; \
+sudo chmod 666 /dev/dri/card0 ;\
+docker run -it \
+-v /var/tmp:/var/tmp \
+--device=/dev/xlnk:/dev/xlnk \
+--device=/dev/dri/card0:/dev/dri/card0 \
+--env KPI_START=$KPI_START \
+$HDDL_CONTAINER /run_test.sh
+
+
