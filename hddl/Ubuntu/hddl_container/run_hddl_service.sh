37d8e4de8d838596e5c64793fafeb5a1299cfa6f
Leow, Chun Fung <chun.fung.leow@intel.com>
2020-09-11 10:27:44 +0800

    delay in hddl connect

--- hddl/Ubuntu/hddl_container/run_hddl_service.sh
+++ hddl/Ubuntu/hddl_container/run_hddl_service.sh
@@ -2,6 +2,7 @@
 rm -rf /var/tmp/mode_set
 rm -rf /tmp/mode_set
 
+sleep 10
 cd /host_install_dir/hvasample
 source /host_install_dir/hvasample/prepare_run.sh
 /host_install_dir/hddlunite/bin/SetHDDLMode -m bypass
