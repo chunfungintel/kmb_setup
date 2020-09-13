452fe3ae5026c8bf55c15c633cb36deb5c895498
Leow, Chun Fung <chun.fung.leow@intel.com>
2020-09-13 13:44:11 +0800

    container run

---
+++ hddl/Test/container_bash.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ -z "$HDDL_IMAGE_NAME" ]; then
+    echo "Need to set HDDL_IMAGE_NAME "
+    exit
+fi
+
+if [ -z "$HDDL_IMAGE_TAG" ]; then
+    echo "Need to set HDDL_IMAGE_TAG "
+    exit
+fi
+
+
+HDDL_CONTAINER=$HDDL_IMAGE_NAME:$HDDL_IMAGE_TAG
+CONTAINER_NAME=TEST01
+KPI_START=5
+
+#sudo chmod 666 /dev/xlnk ; \
+
+docker run -it \
+-v /tmp:/var/tmp \
+--env HVA_TEST_TIMEOUT=30 \
+--device=/dev/xlnk:/dev/xlnk \
+--device=/dev/dri/card0:/dev/dri/card0 \
+--env KPI_START=$KPI_START \
+$HDDL_CONTAINER bash
+
+
