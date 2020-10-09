c9e45d18cef3af0d1ee8908a551025ee260b355f
Leow Chun Fung <chun.fung.leow@intel.com>
2020-10-09 23:44:29 +0700

    rearrange

--- hddl/Ubuntu/hddl_container/benchmark/image_build.sh
+++ hddl/Ubuntu/hddl_container/benchmark/image_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export HDDL_IMAGE_NAME=hddlunite_connect
-export HDDL_IMAGE_TAG=20201003-0611
-export HDDL_RESOURCE_FOLDER=/home/chunfung/CF/Benchmark_WS
-export HOST_PACKAGE_LINK=https://ubit-artifactory-sh.intel.com/artifactory/sed-dgn-local/yocto/builds/2020/PREINT/20201003-0611
-export HOST_PACKAGE_NAME=bypass_host_hddlunite_hvasample_demo-2327426.tgz
-export OPENVINO_PACKAGE_NAME=l_openvino_toolkit_private_ubuntu18_kmb_x86_p_2021.1.0-1237-3cabe58ed07-releases_2020_kmb_pv2.tar.gz
-export DEB_PACKAGE_NAME=kmb-hddl-driver-dkms_0.1.0-eabaa_all.deb
+#export HDDL_IMAGE_NAME=hddlunite_connect
+#export HDDL_IMAGE_TAG=20201003-0611
+#export HDDL_RESOURCE_FOLDER=/home/chunfung/CF/Benchmark_WS
+#export HOST_PACKAGE_LINK=https://ubit-artifactory-sh.intel.com/artifactory/sed-dgn-local/yocto/builds/2020/PREINT/20201003-0611
+#export HOST_PACKAGE_NAME=bypass_host_hddlunite_hvasample_demo-2327426.tgz
+#export OPENVINO_PACKAGE_NAME=l_openvino_toolkit_private_ubuntu18_kmb_x86_p_2021.1.0-1237-3cabe58ed07-releases_2020_kmb_pv2.tar.gz
+#export DEB_PACKAGE_NAME=kmb-hddl-driver-dkms_0.1.0-eabaa_all.deb
 
 if [ -z "$HDDL_IMAGE_NAME" ]; then
     echo "Need to set HDDL_IMAGE_NAME "
@@ -55,6 +55,7 @@ rm -rf $HDDL_RESOURCE_FOLDER/$DEB_PACKAGE_NAME
 wget $HOST_PACKAGE_LINK/$DEB_PACKAGE_NAME -P $HDDL_RESOURCE_FOLDER
 
 cp -r ./scripts/* $HDDL_RESOURCE_FOLDER
+cp ../../../../resources/release_kmb/* $HDDL_RESOURCE_FOLDER/test
 
 docker build \
 --no-cache=true \
