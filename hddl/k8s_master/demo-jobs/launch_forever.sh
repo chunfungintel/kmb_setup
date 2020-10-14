4730123670c937d624d1b724f40e803b6cd49923
Leow, Chun Fung <chun.fung.leow@intel.com>
2020-10-14 14:46:16 +0800

    update

---
+++ hddl/k8s_master/demo-jobs/launch_forever.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+: '
+sameple:
+
+export HDDL_IMAGE_NAME=hddlunite_connect
+export HDDL_IMAGE_TAG=20200912-0857
+export HDDL_TEST_FILE=/run_kpi.sh
+export HDDL_KMB_COUNT=3
+'
+
+if [ -z "$HDDL_IMAGE_NAME" ]; then
+    echo "Need to set HDDL_IMAGE_NAME "
+    exit
+fi
+
+if [ -z "$HDDL_IMAGE_TAG" ]; then
+    echo "Need to set HDDL_IMAGE_TAG "
+    exit
+fi
+
+export HDDL_TEST_FILE=/run_forever.sh
+if [ -z "$HDDL_KMB_COUNT" ]; then
+    export HDDL_KMB_COUNT=3
+fi
+if [ -z "$JOB_TYPE" ]; then
+    export JOB_TYPE=forever
+fi
+
+export RANDOM_STR=`head /dev/urandom | tr -dc a-z0-9 | head -c10`
+echo $RANDOM_STR
+envsubst < deployment.yaml | kubectl apply -f -
