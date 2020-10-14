0740ab971662c596f22d03dbd8e7c6eb43413483
Leow Chun Fung <chun.fung.leow@intel.com>
2020-10-14 23:41:36 +0700

    name correction

--- k3s/master/kmb-full-pipeline-jobs/launch_benchmark.sh
+++ k3s/master/kmb-full-pipeline-jobs/launch_benchmark.sh
@@ -8,7 +8,7 @@ echo $RANDOM_STR
 export JOB_NAME=benchmark
 export VPU_USAGE=high
 export CODEC_USAGE=low
-export KMB_JOB_FILE=/data/launch_benchmark.sh
+export KMB_JOB_FILE=/data/launch_gst_benchmark.sh
 export KMB_VPU_COUNT=1
 export KMB_CODEC_COUNT=1
 
