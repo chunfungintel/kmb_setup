5a2ebcb9c47f9e4516454534614d286d2e8d109f
Leow Chun Fung <chun.fung.leow@intel.com>
2020-10-14 15:22:38 +0700

    device plugin update

--- hddl/Ubuntu/hddl_container/devplugin/image_build.sh
+++ hddl/Ubuntu/hddl_container/devplugin/image_build.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-if [ -z "$HDDL_IMAGE_NAME" ]; then
-    echo "Need to set HDDL_IMAGE_NAME "
+if [ -z "$DEV_PLUGIN_IMAGE_NAME" ]; then
+    echo "Need to set DEV_PLUGIN_IMAGE_NAME "
     exit
 fi
 
-if [ -z "$HDDL_IMAGE_TAG" ]; then
-    echo "Need to set HDDL_IMAGE_TAG "
+if [ -z "$DEV_PLUGIN_IMAGE_TAG" ]; then
+    echo "Need to set DEV_PLUGIN_IMAGE_TAG "
     exit
 fi
 
-if [ -z "$HDDL_RESOURCE_FOLDER" ]; then
-    echo "Need to set HDDL_RESOURCE_FOLDER "
+if [ -z "$DEV_PLUGIN_RESOURCE_FOLDER" ]; then
+    echo "Need to set DEV_PLUGIN_RESOURCE_FOLDER "
     exit
 fi
 
+cd $DEV_PLUGIN_RESOURCE_FOLDER
+rm -rf edge-ai-device-plugin
 git clone https://github.com/intel/edge-ai-device-plugin
 
 docker build \
 --no-cache=false \
 -f ./dockerfiletest \
--t $HDDL_IMAGE_NAME:$HDDL_IMAGE_TAG $HDDL_RESOURCE_FOLDER
+-t $DEV_PLUGIN_IMAGE_NAME:$DEV_PLUGIN_IMAGE_TAG $DEV_PLUGIN_RESOURCE_FOLDER
 
 docker image ls
