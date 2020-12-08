dc61cf6c6a4a4d1ab47efed428b22848ee7f0139
Leow Chun Fung <chun.fung.leow@intel.com>
2020-12-09 07:53:44 +0800

    bench test request

--- resources/launch_gst_benchmark.sh
+++ resources/launch_gst_benchmark.sh
@@ -1,13 +1,20 @@
+export MAX_SUPPORT_VPU=4
+export MAX_SUPPORT_CODEC=4
+
+export VPU_USED=$((100/$MAX_SUPPORT_VPU))
+export CODEC_USED=$((100/$MAX_SUPPORT_CODEC))
+
 oldnum=`cut -d ',' -f2 /data/kmb/vpu`
-newnum=`expr $oldnum + 100`
+newnum=`expr $oldnum + $VPU_USED`
 sed -i "s/$oldnum\$/$newnum/g" /data/kmb/vpu
 
 oldnum=`cut -d ',' -f2 /data/kmb/codec`
-newnum=`expr $oldnum + 100`
+newnum=`expr $oldnum + $CODEC_USED`
 sed -i "s/$oldnum\$/$newnum/g" /data/kmb/codec
 
+
 source /data/source_container.env
-printf "1\n3\n" | XLinkStartStop
+#printf "1\n3\n" | XLinkStartStop
 
 /opt/openvino/deployment_tools/inference_engine/bin/aarch64/benchmark_app -m /data/release_kmb/resnet-50-pytorch.blob -d KMB -nireq 1 -niter 1000 -i /data/textures
 
@@ -15,9 +22,11 @@ printf "1\n3\n" | XLinkStartStop
 sleep 10
 
 oldnum=`cut -d ',' -f2 /data/kmb/vpu`
-newnum=`expr $oldnum - 100`
+newnum=`expr $oldnum - $VPU_USED`
 sed -i "s/$oldnum\$/$newnum/g" /data/kmb/vpu
 
 oldnum=`cut -d ',' -f2 /data/kmb/codec`
-newnum=`expr $oldnum - 100`
+newnum=`expr $oldnum - $CODEC_USED`
 sed -i "s/$oldnum\$/$newnum/g" /data/kmb/codec
+
+
