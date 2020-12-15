0d949f5b300318bae9a0baa74bb5b93a6d2b0085
Leow Chun Fung <chun.fung.leow@intel.com>
2020-12-15 13:39:32 +0700

    TLS deployment

---
+++ k3s/master/Setup_tls.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Launch services
+kubectl create -f /etc/edge-ai/deploy/0-ros-deploy.yaml; \
+sleep 5; \
+sed 's%vpusmm0%vpumgr0%g' /etc/edge-ai/deploy/1-device_plugin_daemonset.yaml | kubectl create -f - ; \
+sleep 5; \
+kubectl create -f /etc/edge-ai/deploy/2-collectd_deploy.yaml; \
+sleep 5; \
+kubectl create -f /etc/edge-ai/deploy/6-prometheus-server.yaml; \
+sleep 5; \
+kubectl create -f /etc/edge-ai/deploy/5-prometheus-adapter.yaml
+
