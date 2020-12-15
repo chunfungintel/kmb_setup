e32901023f32515b1438b05ab252863e2465c9d0
Lim, Poh Keng <pohkeng.lim@intel.com>
2020-12-16 00:21:17 +0800

    Update with config changes

--- k8s/master/kmb-full-pipeline-jobs/launch_full.sh
+++ k8s/master/kmb-full-pipeline-jobs/launch_full.sh
@@ -8,8 +8,8 @@ echo $RANDOM_STR
 export JOB_NAME=full-pipeline
 export VPU_USAGE=low
 export CODEC_USAGE=medium
-export KMB_IMAGE_NAME=gar-registry.caas.intel.com/virtiot/kmb_standalone:latest
-export KMB_JOB_FILE=/data/launch_gst_full.sh
+export KMB_IMAGE_NAME=gar-registry.caas.intel.com/virtiot/kmb_full:20201215-0817
+export KMB_JOB_FILE=/data/launch_gst_multi.sh
 export KMB_VPU_COUNT=1
 export KMB_CODEC_COUNT=1
 
