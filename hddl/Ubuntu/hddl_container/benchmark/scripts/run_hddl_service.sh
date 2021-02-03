3cbba633b7285d681ab81cdfd0fe1d0c1001c11d
Time Leow <chfung2020@yahoo.com>
2021-02-03 17:47:02 +0800

    latest

--- hddl/Ubuntu/hddl_container/benchmark/scripts/run_hddl_service.sh
+++ hddl/Ubuntu/hddl_container/benchmark/scripts/run_hddl_service.sh
@@ -14,6 +14,7 @@ rm -rf /var/tmp/hddlunite_service.sock
 sleep 10
 cd $HDDLUNITE_PATH
 source ./env_host.sh
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HDDLUNITE_PATH}/thirdparty/XLink/lib:${HDDLUNITE_PATH}/thirdparty/Pms/lib
 SetHDDLMode -m bypass
 sleep infinity
 
--- hddl/Ubuntu/hddl_container/benchmark/scripts/run_test.sh
+++ hddl/Ubuntu/hddl_container/benchmark/scripts/run_test.sh
@@ -2,7 +2,7 @@
 
 cd $HVASAMPLE_PATH
 source ./prepare_run.sh
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HDDLUNITE_PATH}/thirdparty/XLink/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HDDLUNITE_PATH}/thirdparty/XLink/lib:${HDDLUNITE_PATH}/thirdparty/Pms/lib
 ./FullPipe &
 
 if [ -z "$HVA_TEST_TIMEOUT" ]; then
