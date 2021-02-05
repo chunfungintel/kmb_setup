df5f133b4d2c660c5e0b0ab4a4d61d20116cae39
root <plim24@intel.com>
2021-02-05 03:33:48 +0000

    Update new blob files

--- k3s/master/kmb-full-pipeline-jobs/version_check.sh
+++ k3s/master/kmb-full-pipeline-jobs/version_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
- export KMB_IMAGE_NAME=gar-registry.caas.intel.com/virtiot/kmb_full:20201013-1636
+ export KMB_IMAGE_NAME=gar-registry.caas.intel.com/virtiot/kmb_full:PV2B0 
+ #export KMB_IMAGE_NAME=gar-registry.caas.intel.com/virtiot/kmb_full:20201013-1636
 
 if [ -z "$KMB_IMAGE_NAME" ]; then
     echo "Need to set KMB_IMAGE_NAME "
