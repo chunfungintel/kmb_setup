15cdaed6213e3a914b4c20acf5d2a3b593ca305c
Time Leow <chfung2020@yahoo.com>
2021-02-24 05:28:17 +0800

    correction

--- hddl/k8s_master/services/launch_devplugin_ver_2.sh
+++ hddl/k8s_master/services/launch_devplugin_ver_2.sh
@@ -2,5 +2,6 @@
 
 
 export DEV_PLUGIN_IMAGE_NAME=gar-registry.caas.intel.com/virtiot/hddl_dev_plugin
-export DEV_PLUGIN_IMAGE_TAG=$HDDL_IMAGE_TAG
+export DEV_PLUGIN_IMAGE_TAG=20210204-1500
+
 envsubst < kmb-vpu-plugin.yaml | kubectl apply -f -
