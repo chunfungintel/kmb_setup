324222ef2a94410a0fd8e88cd3660e15f9c13d67
Leow Chun Fung <chun.fung.leow@intel.com>
2021-02-24 16:18:44 +0700

    image download with username

--- hddl/Ubuntu/hddl_container/benchmark/download_and_build.sh
+++ hddl/Ubuntu/hddl_container/benchmark/download_and_build.sh
@@ -4,6 +4,7 @@
 #export HDDL_IMAGE_TAG=20201013-1636
 #export HDDL_RESOURCE_FOLDER=/home/chunfung/CF/Benchmark_WS4
 #export HOST_PACKAGE_LINK=https://ubit-artifactory-sh.intel.com/artifactory/sed-dgn-local/yocto/builds/2020/Mainline_BKC/20201013-1636
+#export USER_EXTERNAL=chunfung
 
 export KMB_LINK=$HOST_PACKAGE_LINK
 jupyter nbconvert --to python kmb_dl_scripts.ipynb
