68e67980e814514fc7e44cb93d563af6b231588b
Time Leow <chfung2020@yahoo.com>
2021-03-10 14:46:35 +0800

    rearrange

--- hddl/k8s_master/demo-jobs/launch_hva.sh
+++ hddl/k8s_master/demo-jobs/launch_hva.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
 
-: '
-sameple:
-
-export HDDL_IMAGE_NAME=hddlunite_connect
-export HDDL_IMAGE_TAG=20200912-0857
-export HDDL_TEST_FILE=/run_kpi.sh
-export HDDL_KMB_COUNT=3
-'
-
-if [ -z "$HDDL_IMAGE_NAME" ]; then
-    echo "Need to set HDDL_IMAGE_NAME "
-    exit
-fi
-
-if [ -z "$HDDL_IMAGE_TAG" ]; then
-    echo "Need to set HDDL_IMAGE_TAG "
-    exit
-fi
+. ./version_check.sh
 
 export HDDL_TEST_FILE=/run_test.sh
 if [ -z "$HDDL_KMB_COUNT" ]; then
-    export HDDL_KMB_COUNT=3
+    export HDDL_KMB_COUNT=1
 fi
 if [ -z "$JOB_TYPE" ]; then
     export JOB_TYPE=hva
--- hddl/k8s_master/demo-jobs/launch_kpi.sh
+++ hddl/k8s_master/demo-jobs/launch_kpi.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-: '
-sameple:
-
-export HDDL_IMAGE_NAME=hddlunite_connect
-export HDDL_IMAGE_TAG=20200912-0857
-export HDDL_TEST_FILE=/run_kpi.sh
-export HDDL_KMB_COUNT=3
-'
-
-if [ -z "$HDDL_IMAGE_NAME" ]; then
-    echo "Need to set HDDL_IMAGE_NAME "
-    exit
-fi
-
-if [ -z "$HDDL_IMAGE_TAG" ]; then
-    echo "Need to set HDDL_IMAGE_TAG "
-    exit
-fi
+. ./version_check.sh
 
 export HDDL_TEST_FILE=/run_kpi.sh
 if [ -z "$HDDL_KMB_COUNT" ]; then
